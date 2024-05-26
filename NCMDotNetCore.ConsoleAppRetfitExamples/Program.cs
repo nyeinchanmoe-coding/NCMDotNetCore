7686a57a1c748be0015202fa5dbe8e3026860530
Nyein Chan Moe <nyeinchanmoe5899@gmail.com>
2024-05-26 21:35:44 +0630

    Pizza Api, WindowForms

---
+++ NCMDotNetCore.ConsoleAppRetfitExamples/Program.cs
@@ -0,0 +1,17 @@
+﻿using NCMDotNetCore.ConsoleAppRefitExamples;
+using Refit;
+
+//var service = RestService.For<IBlogApi>("https://localhost:7004");
+//var lst = await service.GetBlogs();
+
+//foreach (var item in lst)
+//{
+//    Console.WriteLine($"BlogId => {(item.BlogId)}");
+//    Console.WriteLine($"BlogTitle => {(item.BlogTitle)}");
+//    Console.WriteLine($"BlogAuthor => {(item.BlogAuthor)}");
+//    Console.WriteLine($"BlogContent => {(item.BlogContent)}");
+//    Console.WriteLine("*************************");
+//}
+
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
---
+++ NCMDotNetCore.WinFormsApp/FrmBlog.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NCMDotNetCore.WinFormsApp
+{
+    partial class FrmBlog
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTitle = new TextBox();
+            txtAuthor = new TextBox();
+            txtContent = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnCancel = new Button();
+            btnSave = new Button();
+            SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(71, 56);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(100, 25);
+            txtTitle.TabIndex = 1;
+            // 
+            // txtAuthor
+            // 
+            txtAuthor.Location = new Point(71, 129);
+            txtAuthor.Name = "txtAuthor";
+            txtAuthor.Size = new Size(100, 25);
+            txtAuthor.TabIndex = 2;
+            // 
+            // txtContent
+            // 
+            txtContent.Location = new Point(71, 212);
+            txtContent.Multiline = true;
+            txtContent.Name = "txtContent";
+            txtContent.Size = new Size(337, 64);
+            txtContent.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(71, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 19);
+            label1.TabIndex = 4;
+            label1.Text = "Title :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(71, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 19);
+            label2.TabIndex = 5;
+            label2.Text = "Author :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(71, 174);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 19);
+            label3.TabIndex = 6;
+            label3.Text = "Content :";
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = Color.FromArgb(84, 110, 122);
+            btnCancel.FlatAppearance.BorderSize = 0;
+            btnCancel.FlatStyle = FlatStyle.Flat;
+            btnCancel.ForeColor = Color.White;
+            btnCancel.Location = new Point(71, 295);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 31);
+            btnCancel.TabIndex = 7;
+            btnCancel.Text = "C&ancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.FromArgb(67, 160, 71);
+            btnSave.FlatAppearance.BorderSize = 0;
+            btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(169, 295);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 31);
+            btnSave.TabIndex = 8;
+            btnSave.Text = "&Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // FrmBlog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 510);
+            Controls.Add(btnSave);
+            Controls.Add(btnCancel);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtContent);
+            Controls.Add(txtAuthor);
+            Controls.Add(txtTitle);
+            Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.Black;
+            Name = "FrmBlog";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Blog";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txtTitle;
+        private TextBox txtAuthor;
+        private TextBox txtContent;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnCancel;
+        private Button btnSave;
+    }
+}
